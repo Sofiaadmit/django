8281cf430b2ca5b57f1b64324016a38daa191c60
SofiaCruz <isabele.sofia23@gmail.com>
2026-06-09 20:21:32 -0300

    Adicionando views baseadas em funções

--- meuapp/views.py
+++ meuapp/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    nome = request.GET.get("nome")
+    contexto = { 
+        "nome": nome
+    }
+    return render(request, 'index.html', contexto)
 
 def sobre(request):
     return render(request, 'sobre.html')
 
 def contato(request):
     return render(request, 'contato.html')
+
+def aluno(request, id_aluno):
+    contexto = {
+        "id_aluno": id_aluno
+    }
+    return render(request, "aluno.html", contexto)
